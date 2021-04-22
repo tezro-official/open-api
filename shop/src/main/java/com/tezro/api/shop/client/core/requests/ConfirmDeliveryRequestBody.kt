d7148332b3fc96e89305901bfe18ad78b6b6a82c
Tamim Attafi <tamimattafi@iMac-Tamim.local>
2021-04-22 19:37:59 +0700

    feat: Added retrofit client and prepared retrofit client requests and bodies

---
+++ shop/src/main/java/com/tezro/api/shop/client/core/requests/ConfirmDeliveryRequestBody.kt
@@ -0,0 +1,5 @@
+package com.tezro.api.shop.client.core.requests
+
+internal data class ConfirmDeliveryRequestBody(
+    val comment: String?
+)
---
+++ shop/src/main/java/com/tezro/api/shop/client/core/requests/SendMessageRequestBody.kt
@@ -0,0 +1,5 @@
+package com.tezro.api.shop.client.core.requests
+
+internal data class SendMessageRequestBody(
+    val message: String
+)
